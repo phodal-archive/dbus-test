b528124015820aa93fc03529e0739f8760bd63b1
Phodal <h@phodal.com>
2016-08-22 09:31:52 +0800

    format code

--- gtk-demo.py
+++ gtk-demo.py
@@ -1,37 +1,38 @@
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+
 class MyWindow(Gtk.Window):
-
     def __init__(self):
-	Gtk.Window.__init__(self, title="Hello World")
-	self.set_size_request(200, 100)
+        Gtk.Window.__init__(self, title="Hello World")
+        self.set_size_request(200, 100)
 
-
-	vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)	
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
 
         self.entry = Gtk.Entry()
-	self.entry.set_text('Hello World')
+        self.entry.set_text('Hello World')
         vbox.pack_start(self.entry, True, True, 0)
 
-	hbox = Gtk.Box(spacing=6)
+        hbox = Gtk.Box(spacing=6)
         vbox.pack_start(hbox, True, True, 0)
 
-	self.button = Gtk.Button(label="connect")
-	self.button.connect("clicked", self.connect_server)
+        self.button = Gtk.Button(label="connect")
+        self.button.connect("clicked", self.connect_server)
         hbox.pack_start(self.button, True, True, 0)
 
-	self.button = Gtk.Button(label="message")
-	self.button.connect("clicked", self.send_message)
+        self.button = Gtk.Button(label="message")
+        self.button.connect("clicked", self.send_message)
         hbox.pack_start(self.button, True, True, 0)
 
     def connect_server(self, widget):
-	print("Hello World")
+        print("Hello World")
 
     def send_message(self, widget):
-	print("Send Message")
+        print("Send Message")
+
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
