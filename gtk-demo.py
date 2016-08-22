3f2c0036f7190b531c4bfa69afe904850c20fe4f
Fengda HUANG <h@phodal.com>
2016-08-22 11:37:30 +0800

    Extract files

--- gtk-demo.py
+++ gtk-demo.py
@@ -1,5 +1,7 @@
 import gi
 
+from account import MyBuddyCallback, MyAccountCallback
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 import pjsua as pj
@@ -29,51 +31,11 @@ class DialogExample(Gtk.Dialog):
         self.show_all()
 
 
-class MyAccountCallback(pj.AccountCallback):
-    def __init__(self, account=None):
-        pj.AccountCallback.__init__(self, account)
-
-    def on_incoming_subscribe(self, buddy, from_uri, contact_uri, pres):
-        global pending_pres, pending_uri
-        # Allow buddy to subscribe to our presence
-        if buddy:
-            return (200, None)
-        print 'Incoming SUBSCRIBE request from', from_uri
-        print 'Press "A" to accept and add, "R" to reject the request'
-        pending_pres = pres
-        pending_uri = from_uri
-        return (202, None)
-
-
-class MyBuddyCallback(pj.BuddyCallback):
-    def __init__(self, buddy=None):
-        pj.BuddyCallback.__init__(self, buddy)
-
-    def on_state(self):
-        print "Buddy", self.buddy.info().uri, "is",
-        print self.buddy.info().online_text
-
-    def on_pager(self, mime_type, body):
-        print "Instant message from", self.buddy.info().uri,
-        print "(", mime_type, "):"
-        print body
-
-    def on_pager_status(self, body, im_id, code, reason):
-        if code >= 300:
-            print "Message delivery failed for message",
-            print body, "to", self.buddy.info().uri, ":", reason
-
-    def on_typing(self, is_typing):
-        if is_typing:
-            print self.buddy.info().uri, "is typing"
-        else:
-            print self.buddy.info().uri, "stops typing"
-
-
 class MyWindow(Gtk.Window):
     def __init__(self):
         self.draw_ui()
         self.init_pjsua()
+
 
     def init_pjsua(self):
         lib = pj.Lib()
