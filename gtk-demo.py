15a69b982a1e645c096797456a6afa5502690715
Fengda HUANG <h@phodal.com>
2016-08-22 11:33:26 +0800

    change text to button

--- gtk-demo.py
+++ gtk-demo.py
@@ -1,21 +1,23 @@
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 import pjsua as pj
 
 LOG_LEVEL = 3
 pending_pres = None
 pending_uri = None
 
+
 def log_cb(level, str, len):
     print str,
+
 
 class DialogExample(Gtk.Dialog):
     def __init__(self, parent):
         Gtk.Dialog.__init__(self, "My Dialog", parent, 0,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OK, Gtk.ResponseType.OK))
 
         self.set_default_size(150, 100)
 
@@ -24,6 +26,7 @@ class DialogExample(Gtk.Dialog):
         box = self.get_content_area()
         box.add(label)
         self.show_all()
+
 
 class MyAccountCallback(pj.AccountCallback):
     def __init__(self, account=None):
@@ -65,18 +68,23 @@ class MyBuddyCallback(pj.BuddyCallback):
         else:
             print self.buddy.info().uri, "stops typing"
 
+
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello World")
         self.set_size_request(200, 100)
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(vbox)
 
-        sip_address_label = Gtk.Label()
-        sip_address_label.set_text("SIP Address")
-        sip_address_label.set_justify(Gtk.Justification.LEFT)
-        vbox.pack_start(sip_address_label, True, True, 0)
+        self.sip_address_label = Gtk.Button()
+        self.sip_address_label.set_label("SIP Address")
+
+        self.sip_address_label.connect("clicked", self.copy_text)
+        self.clipboard.set_text(self.sip_address_label.get_label(), -1)
+
+        vbox.pack_start(self.sip_address_label, True, True, 0)
 
         self.sip_text = Gtk.Entry()
         self.sip_text.set_text('sip')
@@ -118,9 +126,9 @@ class MyWindow(Gtk.Window):
             self.acc.set_basic_status(True)
 
             self.my_sip_uri = "sip:" + transport.info().host + \
-                         ":" + str(transport.info().port)
+                              ":" + str(transport.info().port)
 
-            sip_address_label.set_text(self.my_sip_uri)
+            self.sip_address_label.set_label(self.my_sip_uri)
             self.buddy = None
 
         except pj.Error, e:
@@ -128,12 +136,16 @@ class MyWindow(Gtk.Window):
             lib.destroy()
             lib = None
 
+    def copy_text(self, widget):
+        self.clipboard.set_text(self.sip_address_label.get_label(), -1)
+
     def connect_server(self, widget):
         self.buddy = self.acc.add_buddy(self.my_sip_uri, cb=MyBuddyCallback())
         self.buddy.subscribe()
 
     def send_message(self, widget):
         print("Send Message")
+
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
